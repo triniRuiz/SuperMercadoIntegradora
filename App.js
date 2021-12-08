6fce9e0f7232a99635676259ea8af4b07315d91c
triniRuiz <72765513+triniRuiz@users.noreply.github.com>
2021-12-08 14:40:34 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react';
+import Navegacion from './app/navigations/Navegacion';
+import {firebaseApp} from './app/utils/firebase';
+import * as firebase from "firebase/auth";
+
+export default function App() {
+    useEffect(() =>{
+
+    });
+
+  return <Navegacion/>
+}
+
+
